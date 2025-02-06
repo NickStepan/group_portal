50ff066431f21d34fa4746c107eee162566239ca
Kiwindonix <vitia.vasylenko@gmail.com>
2025-02-06 18:27:09 +0200

    .

--- auth_system/views.py
+++ auth_system/views.py
@@ -2,23 +2,29 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
 
+class UserRegisterView(CreateView):
+    form_class = UserCreationForm
+    template_name = template_name="auth_system/register.html"
+    success_url = reverse_lazy("index")
 
-def register(request):
-    if request.method == "POST":
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect("index")
-    else:
-        form = UserCreationForm()
+# def register(request):
+#     if request.method == "POST":
+#         form = UserCreationForm(request.POST)
+#         if form.is_valid():
+#             user = form.save()
+#             login(request, user)
+#             return redirect("index")
+#     else:
+#         form = UserCreationForm()
 
-    return render(
-        request,
-        template_name="auth_system/register.html",
-        context = {"form": form}
-    )
+#     return render(
+#         request,
+#         template_name="auth_system/register.html",
+#         context = {"form": form}
+#     )
 
 # def login(request):
 #     if request.method == "POST":

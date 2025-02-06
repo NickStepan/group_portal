0c9cb19264f332f437b877a17e4d5f41ed2aa2f2
Vladtopchik <vladkyriniq@gmail.com>
2025-02-06 18:06:16 +0200

    Створено додаток main та інше.

--- auth_system/admin.py
+++ auth_system/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from auth_system.models import CustomUser
 
 #адмінка: admin пароль 1
+admin.site.register(CustomUser)
--- auth_system/models.py
+++ auth_system/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+def get_default_dict():
+    return {
+        'theme': 'light',
+    }
+
+
+class CustomUser(AbstractUser):
+    settings = models.JSONField(default=get_default_dict)

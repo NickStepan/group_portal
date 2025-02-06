22fff2a372842b15c0e99000c5697baf9d2fcd76
Vladtopchik <vladkyriniq@gmail.com>
2025-02-06 18:40:27 +0200

    Створено додаток main та інше.

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,40 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
-from django.contrib.auth import login, authenticate
-from django.contrib import messages
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
+
 
 class UserRegisterView(CreateView):
     form_class = UserCreationForm
-    template_name = template_name="auth_system/register.html"
+    template_name = "auth_system/register.html"
     success_url = reverse_lazy("index")
 
-# def register(request):
-#     if request.method == "POST":
-#         form = UserCreationForm(request.POST)
-#         if form.is_valid():
-#             user = form.save()
-#             login(request, user)
-#             return redirect("index")
-#     else:
-#         form = UserCreationForm()
 
-#     return render(
-#         request,
-#         template_name="auth_system/register.html",
-#         context = {"form": form}
-#     )
-
-# def login(request):
-#     if request.method == "POST":
-#         form =  AuthenticationForm(request, data=request.POST)
-#         if form.is_valid():
-#             username = form.cleaned_data.get("username")
-#             password = form.cleaned_data.get("password")
-#             user = authenticate(request, username=username, password=password)
-#             if user is not None:
-#                 login(request, user)
-#                 return redirect("index")
-#             else:
-#                 messages.error(request, "Неправильний логін та пароль.")
+class UserLoginView(LoginView):
+    template_name = 'accounts/login.html'
+    authentication_form = AuthenticationForm
+    success_url = reverse_lazy('task-list')

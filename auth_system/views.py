8b55f784ea49f2111b15342e0c8cea3f2ea10760
Kiwindonix <vitia.vasylenko@gmail.com>
2025-02-08 19:31:34 +0200

    .

--- auth_system/views.py
+++ auth_system/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.views.generic import CreateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 
 
@@ -12,6 +12,9 @@ class UserRegisterView(CreateView):
 
 
 class UserLoginView(LoginView):
-    template_name = 'accounts/login.html'
+    template_name = 'auth_system/login.html'
     authentication_form = AuthenticationForm
     success_url = reverse_lazy('task-list')
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('index')

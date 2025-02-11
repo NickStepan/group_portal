e2b6d457fd001e5e64b8889d05fd67cf5cc23889
Vladtopchik <vladkyriniq@gmail.com>
2025-02-11 22:30:48 +0200

    обота на he theme

--- static/js/index.js
+++ static/js/index.js
@@ -2,6 +2,7 @@ const logo = document.getElementById('logo');
 const logodiv = logo.querySelector('div');
 const search = document.querySelector('search input');
 const clear = document.getElementById('clear');
+const menu = document.getElementById('menu');
 
 
 let animation = lottie.loadAnimation({
@@ -12,7 +13,23 @@ let animation = lottie.loadAnimation({
     path: `${static}graphics/lottie/unitedflow.json`
 });
 
-animation.addEventListener('DOMLoaded', function() {
+let menuanim = lottie.loadAnimation({
+    container: menu,
+    renderer: 'svg',  
+    loop: false,     
+    autoplay: false, 
+    path: `${static}graphics/lottie/menu-f.json`
+});
+
+let menuanim2 = lottie.loadAnimation({
+    container: menu,
+    renderer: 'svg',   
+    loop: false,     
+    autoplay: false, 
+    path: `${static}graphics/lottie/menu-b.json`
+});
+
+animation.addEventListener('DOMLoaded', () => {
     let svgElement = animation.renderer.svgElement;
 
     if (svgElement) {
@@ -22,11 +39,34 @@ animation.addEventListener('DOMLoaded', function() {
     }
 });
 
+menuanim2.addEventListener('DOMLoaded', () => {
+    let svgElement = menuanim2.renderer.svgElement;
+    if (svgElement) {
+        svgElement.style.display = 'none';
+    }
+});
+
 let isplaying = false;
 
 animation.addEventListener('complete', function() {
     isplaying = false;
     animation.goToAndStop(0, true);
+});
+
+menuanim.addEventListener('complete', () => {
+    isplayingmenu = false;
+    opened = true;
+    menuanim.renderer.svgElement.style.display = 'none';
+    menuanim2.renderer.svgElement.style.display = 'block';
+    menuanim2.goToAndStop(0, true);
+});
+
+menuanim2.addEventListener('complete', () => {
+    isplayingmenu = false;
+    opened = false;
+    menuanim.renderer.svgElement.style.display = 'block';
+    menuanim2.renderer.svgElement.style.display = 'none';
+    menuanim.goToAndStop(0, true);
 });
 
 logo.addEventListener('mouseenter', () => {
@@ -48,4 +88,18 @@ search.addEventListener('blur', () => {
 clear.addEventListener('click', () => {
     search.value = '';
     search.focus();
+});
+
+let opened = false;
+let isplayingmenu = false;
+
+menu.addEventListener('click', () => {
+    if (!isplayingmenu) {
+        isplayingmenu = true;
+        if (!opened) {
+            menuanim.play()
+        } else {
+            menuanim2.play();
+        }
+    }
 });

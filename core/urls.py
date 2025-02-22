d896dadd827bda9795cb0462809f25bd4e4dd179
Offguard1337 <aleksandrkzchk@gmail.com>
2025-02-22 20:20:53 +0100

    .

--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,5 +26,5 @@ urlpatterns = [
     path('', include('main.urls')),
     path('', include('diary.urls')),
     path('calendar/', include('calendar_events.urls')),  # Підключаємо маршрути календаря
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 

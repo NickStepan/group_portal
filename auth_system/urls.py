f68804d539da02f72002acc4fe468759247c4304
NickStepan <NickStepan>
2025-03-16 18:30:06 +0200

    new diaryasdasdasd

--- auth_system/urls.py
+++ auth_system/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("register/", views.UserRegisterView.as_view(), name="register"),
     path("login/", views.UserLoginView.as_view(), name="login"),
     path('logout/', views.UserLogoutView.as_view(next_page='/'), name='logout'),
-    path("profile/", views.ProfileView.as_view(), name="profile")
-    
-
+    path("profile/", views.ProfileView.as_view(), name="profile"),
+    path("update-profile/", views.update_profile, name="update_profile"),
 ]
